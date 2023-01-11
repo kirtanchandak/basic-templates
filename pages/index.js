1c94e7cff6874c13357923113ec8d978f026ec81
kirtanchandak <themarketingkirtan@gmail.com>
2023-01-11 23:09:34 +0530

    header added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "./components/Header";
 
 export default function Home() {
   return (
@@ -13,7 +12,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>kirtan</main>
+      <main className={styles.main}>
+        <Header />
+      </main>
     </>
   );
 }
